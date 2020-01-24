8ed92898d5a8fdf292d1e67d5098b2f83e45ff4e
Derek Milne <derek.m.m.77@gmail.com>
2020-01-23 20:33:56 -0800

    add prizeId

--- lib/dart/winner.service.pb.dart
+++ lib/dart/winner.service.pb.dart
@@ -37,15 +37,16 @@ class GetWinnersResponse extends $pb.GeneratedMessage {
 
 class Winner extends $pb.GeneratedMessage {
   static final $pb.BuilderInfo _i = $pb.BuilderInfo('Winner', package: const $pb.PackageName('api'), createEmptyInstance: create)
-    ..aOS(1, 'prizeTitle', protoName: 'prizeTitle')
-    ..a<$core.double>(2, 'prizeMarketPrice', $pb.PbFieldType.OF, protoName: 'prizeMarketPrice')
-    ..pPS(3, 'prizePhotos', protoName: 'prizePhotos')
-    ..a<$fixnum.Int64>(4, 'guessCount', $pb.PbFieldType.OU6, protoName: 'guessCount', defaultOrMaker: $fixnum.Int64.ZERO)
-    ..aOS(5, 'riddle')
-    ..aOS(6, 'acceptedAnswer', protoName: 'acceptedAnswer')
-    ..aInt64(7, 'timeOfWin', protoName: 'timeOfWin')
-    ..a<$fixnum.Int64>(8, 'winnerId', $pb.PbFieldType.OU6, protoName: 'winnerId', defaultOrMaker: $fixnum.Int64.ZERO)
-    ..aOS(9, 'winnerName', protoName: 'winnerName')
+    ..a<$fixnum.Int64>(1, 'prizeId', $pb.PbFieldType.OU6, protoName: 'prizeId', defaultOrMaker: $fixnum.Int64.ZERO)
+    ..aOS(2, 'prizeTitle', protoName: 'prizeTitle')
+    ..a<$core.double>(3, 'prizeMarketPrice', $pb.PbFieldType.OF, protoName: 'prizeMarketPrice')
+    ..pPS(4, 'prizePhotos', protoName: 'prizePhotos')
+    ..a<$fixnum.Int64>(5, 'guessCount', $pb.PbFieldType.OU6, protoName: 'guessCount', defaultOrMaker: $fixnum.Int64.ZERO)
+    ..aOS(6, 'riddle')
+    ..aOS(7, 'acceptedAnswer', protoName: 'acceptedAnswer')
+    ..aInt64(8, 'timeOfWin', protoName: 'timeOfWin')
+    ..a<$fixnum.Int64>(9, 'winnerId', $pb.PbFieldType.OU6, protoName: 'winnerId', defaultOrMaker: $fixnum.Int64.ZERO)
+    ..aOS(10, 'winnerName', protoName: 'winnerName')
     ..hasRequiredFields = false
   ;
 
@@ -65,78 +66,87 @@ class Winner extends $pb.GeneratedMessage {
   static Winner _defaultInstance;
 
   @$pb.TagNumber(1)
-  $core.String get prizeTitle => $_getSZ(0);
+  $fixnum.Int64 get prizeId => $_getI64(0);
   @$pb.TagNumber(1)
-  set prizeTitle($core.String v) { $_setString(0, v); }
+  set prizeId($fixnum.Int64 v) { $_setInt64(0, v); }
   @$pb.TagNumber(1)
-  $core.bool hasPrizeTitle() => $_has(0);
+  $core.bool hasPrizeId() => $_has(0);
   @$pb.TagNumber(1)
-  void clearPrizeTitle() => clearField(1);
+  void clearPrizeId() => clearField(1);
 
   @$pb.TagNumber(2)
-  $core.double get prizeMarketPrice => $_getN(1);
+  $core.String get prizeTitle => $_getSZ(1);
   @$pb.TagNumber(2)
-  set prizeMarketPrice($core.double v) { $_setFloat(1, v); }
+  set prizeTitle($core.String v) { $_setString(1, v); }
   @$pb.TagNumber(2)
-  $core.bool hasPrizeMarketPrice() => $_has(1);
+  $core.bool hasPrizeTitle() => $_has(1);
   @$pb.TagNumber(2)
-  void clearPrizeMarketPrice() => clearField(2);
+  void clearPrizeTitle() => clearField(2);
 
   @$pb.TagNumber(3)
-  $core.List<$core.String> get prizePhotos => $_getList(2);
+  $core.double get prizeMarketPrice => $_getN(2);
+  @$pb.TagNumber(3)
+  set prizeMarketPrice($core.double v) { $_setFloat(2, v); }
+  @$pb.TagNumber(3)
+  $core.bool hasPrizeMarketPrice() => $_has(2);
+  @$pb.TagNumber(3)
+  void clearPrizeMarketPrice() => clearField(3);
 
   @$pb.TagNumber(4)
-  $fixnum.Int64 get guessCount => $_getI64(3);
-  @$pb.TagNumber(4)
-  set guessCount($fixnum.Int64 v) { $_setInt64(3, v); }
-  @$pb.TagNumber(4)
-  $core.bool hasGuessCount() => $_has(3);
-  @$pb.TagNumber(4)
-  void clearGuessCount() => clearField(4);
+  $core.List<$core.String> get prizePhotos => $_getList(3);
 
   @$pb.TagNumber(5)
-  $core.String get riddle => $_getSZ(4);
+  $fixnum.Int64 get guessCount => $_getI64(4);
   @$pb.TagNumber(5)
-  set riddle($core.String v) { $_setString(4, v); }
+  set guessCount($fixnum.Int64 v) { $_setInt64(4, v); }
   @$pb.TagNumber(5)
-  $core.bool hasRiddle() => $_has(4);
+  $core.bool hasGuessCount() => $_has(4);
   @$pb.TagNumber(5)
-  void clearRiddle() => clearField(5);
+  void clearGuessCount() => clearField(5);
 
   @$pb.TagNumber(6)
-  $core.String get acceptedAnswer => $_getSZ(5);
+  $core.String get riddle => $_getSZ(5);
   @$pb.TagNumber(6)
-  set acceptedAnswer($core.String v) { $_setString(5, v); }
+  set riddle($core.String v) { $_setString(5, v); }
   @$pb.TagNumber(6)
-  $core.bool hasAcceptedAnswer() => $_has(5);
+  $core.bool hasRiddle() => $_has(5);
   @$pb.TagNumber(6)
-  void clearAcceptedAnswer() => clearField(6);
+  void clearRiddle() => clearField(6);
 
   @$pb.TagNumber(7)
-  $fixnum.Int64 get timeOfWin => $_getI64(6);
+  $core.String get acceptedAnswer => $_getSZ(6);
   @$pb.TagNumber(7)
-  set timeOfWin($fixnum.Int64 v) { $_setInt64(6, v); }
+  set acceptedAnswer($core.String v) { $_setString(6, v); }
   @$pb.TagNumber(7)
-  $core.bool hasTimeOfWin() => $_has(6);
+  $core.bool hasAcceptedAnswer() => $_has(6);
   @$pb.TagNumber(7)
-  void clearTimeOfWin() => clearField(7);
+  void clearAcceptedAnswer() => clearField(7);
 
   @$pb.TagNumber(8)
-  $fixnum.Int64 get winnerId => $_getI64(7);
+  $fixnum.Int64 get timeOfWin => $_getI64(7);
   @$pb.TagNumber(8)
-  set winnerId($fixnum.Int64 v) { $_setInt64(7, v); }
+  set timeOfWin($fixnum.Int64 v) { $_setInt64(7, v); }
   @$pb.TagNumber(8)
-  $core.bool hasWinnerId() => $_has(7);
+  $core.bool hasTimeOfWin() => $_has(7);
   @$pb.TagNumber(8)
-  void clearWinnerId() => clearField(8);
+  void clearTimeOfWin() => clearField(8);
 
   @$pb.TagNumber(9)
-  $core.String get winnerName => $_getSZ(8);
+  $fixnum.Int64 get winnerId => $_getI64(8);
   @$pb.TagNumber(9)
-  set winnerName($core.String v) { $_setString(8, v); }
+  set winnerId($fixnum.Int64 v) { $_setInt64(8, v); }
   @$pb.TagNumber(9)
-  $core.bool hasWinnerName() => $_has(8);
+  $core.bool hasWinnerId() => $_has(8);
   @$pb.TagNumber(9)
-  void clearWinnerName() => clearField(9);
+  void clearWinnerId() => clearField(9);
+
+  @$pb.TagNumber(10)
+  $core.String get winnerName => $_getSZ(9);
+  @$pb.TagNumber(10)
+  set winnerName($core.String v) { $_setString(9, v); }
+  @$pb.TagNumber(10)
+  $core.bool hasWinnerName() => $_has(9);
+  @$pb.TagNumber(10)
+  void clearWinnerName() => clearField(10);
 }
 
--- lib/dart/winner.service.pbjson.dart
+++ lib/dart/winner.service.pbjson.dart
@@ -15,15 +15,16 @@ const GetWinnersResponse$json = const {
 const Winner$json = const {
   '1': 'Winner',
   '2': const [
-    const {'1': 'prizeTitle', '3': 1, '4': 1, '5': 9, '10': 'prizeTitle'},
-    const {'1': 'prizeMarketPrice', '3': 2, '4': 1, '5': 2, '10': 'prizeMarketPrice'},
-    const {'1': 'prizePhotos', '3': 3, '4': 3, '5': 9, '10': 'prizePhotos'},
-    const {'1': 'guessCount', '3': 4, '4': 1, '5': 4, '10': 'guessCount'},
-    const {'1': 'riddle', '3': 5, '4': 1, '5': 9, '10': 'riddle'},
-    const {'1': 'acceptedAnswer', '3': 6, '4': 1, '5': 9, '10': 'acceptedAnswer'},
-    const {'1': 'timeOfWin', '3': 7, '4': 1, '5': 3, '10': 'timeOfWin'},
-    const {'1': 'winnerId', '3': 8, '4': 1, '5': 4, '10': 'winnerId'},
-    const {'1': 'winnerName', '3': 9, '4': 1, '5': 9, '10': 'winnerName'},
+    const {'1': 'prizeId', '3': 1, '4': 1, '5': 4, '10': 'prizeId'},
+    const {'1': 'prizeTitle', '3': 2, '4': 1, '5': 9, '10': 'prizeTitle'},
+    const {'1': 'prizeMarketPrice', '3': 3, '4': 1, '5': 2, '10': 'prizeMarketPrice'},
+    const {'1': 'prizePhotos', '3': 4, '4': 3, '5': 9, '10': 'prizePhotos'},
+    const {'1': 'guessCount', '3': 5, '4': 1, '5': 4, '10': 'guessCount'},
+    const {'1': 'riddle', '3': 6, '4': 1, '5': 9, '10': 'riddle'},
+    const {'1': 'acceptedAnswer', '3': 7, '4': 1, '5': 9, '10': 'acceptedAnswer'},
+    const {'1': 'timeOfWin', '3': 8, '4': 1, '5': 3, '10': 'timeOfWin'},
+    const {'1': 'winnerId', '3': 9, '4': 1, '5': 4, '10': 'winnerId'},
+    const {'1': 'winnerName', '3': 10, '4': 1, '5': 9, '10': 'winnerName'},
   ],
 };
 
